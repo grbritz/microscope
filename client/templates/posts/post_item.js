8f7743fcc4c6d035c7f41eabaeff4520057394ac
Graeme Britz <grbritz@gmail.com>
2015-03-27 19:45:22 -0700

    added edit posts form and time of posting to display

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -3,5 +3,17 @@ Template.postItem.helpers({
     var a = document.createElement('a');
     a.href = this.url;
     return a.hostname;
+  },
+  whenSubmitted: function() {
+    if(this.submitted) {
+      return moment(this.submitted).fromNow();
+    }
+    else {
+      return "before the beginning of time";
+    }
+    
+  },
+  ownPost: function() {
+    return this.userId === Meteor.userId();
   }
 });
