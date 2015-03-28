b6b19739a111e098f31bf50e1f1763c51d1bad30
Graeme Britz <grbritz@gmail.com>
2015-03-28 10:40:36 -0700

    basic error package thats linked in

--- client/helpers/errors.js
+++
@@ -1,6 +0,0 @@
-// Null as name since this data will never be saved, and thus needs no mongodb collection
-Errors = new Mongo.Collection(null);
-
-throwError = function(message) {
-  Errors.insert({message: message});
-}
