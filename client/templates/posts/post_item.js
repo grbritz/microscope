cbc9b116697124584bb3f8a96095c23537317b8b
Graeme Britz <grbritz@gmail.com>
2015-03-29 10:42:58 -0700

    greyed out upvote link when not logged in

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -10,16 +10,24 @@ Template.postItem.helpers({
     }
     else {
       return "before the beginning of time";
-    }
-    
+    }  
   },
   ownPost: function() {
     return this.userId === Meteor.userId();
+  },
+  upvotedBtnClass: function() {
+    var userId = Meteor.userId();
+    if (userId && !_.include(this.upvoters, userId)) {
+      return 'btn-primary upvotable';
+    }
+    else {
+      return 'disabled';
+    }
   }
 });
 
 Template.postItem.events({
-  'click .upvote': function(e) {
+  'click .upvotable': function(e) {
     e.preventDefault();
     Meteor.call('upvote', this._id);
   }
